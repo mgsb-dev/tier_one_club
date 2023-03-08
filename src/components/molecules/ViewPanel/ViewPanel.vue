1c4d17192eebbb3e9f8fd9d4290c0421f929c77d
mgsb-dev <mgrace.shyu@gmail.com>
2023-03-08 21:35:38 +0100

    Instaled animate.css and added to views

--- src/components/molecules/ViewPanel/ViewPanel.vue
+++ src/components/molecules/ViewPanel/ViewPanel.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="ViewPanel">
     <div class="ViewPanel__spacer"></div>
-    <div class="ViewPanel__container">
+    <div class="ViewPanel__container animate__animated animate__fadeInUpBig">
       <slot></slot>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, onBeforeUnmount, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+  }
+})
+</script>
 <style lang="sass">
 @import './ViewPanel.sass'
 </style>
--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <main class="Home__wrapper">
-    <div class="Home__first-container">
+    <div class="Home__first-container animate__animated animate__slideInLeft">
       <div class="Home__title-container">
-        <h1 class="Home__title-one">TIER</h1>
+        <h1 class="Home__title-one animate__animated animate__slideInLeft animate__slow">TIER</h1>
       </div>
     </div>
 
     <div class="Home__second-container">
-      <h1 class="Home__title">ONE</h1>
+      <h1 class="Home__title animate__animated animate__slideInRight animate__slow">ONE</h1>
     </div>
   </main>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import 'animate.css'
+</script>
 
 <style lang="sass">
 @import './HomeView.sass'
