8ae7cd0917791e168078a0846e669c8e51138060
mgsb-dev <mgrace.shyu@gmail.com>
2023-03-10 23:08:06 +0100

    Updated components

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VCalendar from 'v-calendar'
 
 import './assets/base.css'
 import '@/styles/index.sass'
 import 'animate.css'
-
+import 'v-calendar/dist/style.css'
 
 const app = createApp(App)
+
+app.use(VCalendar, {})
 
 app.use(router)
 
--- src/views/CalendarView/CalendarView.vue
+++ src/views/CalendarView/CalendarView.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="Calendar">
-    <ViewPanel>
-      <h2 class="Calendar__title">This is Calendar page</h2>
-    </ViewPanel>
+    <h2 class="Calendar__title">Calendar</h2>
+    <Calendar :attributes="attributes" is-dark is-expanded :columns="2" :step="1" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import ViewPanel from '@/components/molecules/ViewPanel/ViewPanel.vue'
+import { Calendar } from 'v-calendar'
+
 export default defineComponent({
-  components: { ViewPanel },
+  components: { Calendar },
   setup() {
-    return
+    const importantDate = 'Important date'
+    const availableDate = 'Available date'
+
+    const dateInformation = [
+      {
+        key: 'today',
+        highlight: 'gray',
+        date: new Date(),
+        description: 'Today'
+      },
+      {
+        description: importantDate,
+        date: 'March 17, 2023',
+        dot: 'red'
+      },
+      {
+        description: availableDate,
+        date: 'March 27, 2023',
+        dot: 'green'
+      }
+    ]
+
+    const attributes = dateInformation.map((el) => ({
+      key: el.date,
+      dates: new Date(el.date),
+      dot: el.dot,
+      highlight: el.highlight,
+      popover: {
+        label: el.description
+      }
+    }))
+
+    return { attributes }
   }
 })
 </script>
